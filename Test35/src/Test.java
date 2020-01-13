d7aa1a89682f8df5aaaa11b583b1ea5ed32e3d05
yunjiayu <386902834@qq.com>
2020-01-13 13:31:24 +0800

    顺序表中新增元素 add

---
+++ Test35/src/Test.java
@@ -0,0 +1,11 @@
+public class Test {
+    public static void main(String[] args) {
+        SeqList seqList = new SeqList();
+        seqList.add(0,100);
+        seqList.add(0,200);
+        seqList.add(0,300);
+        seqList.add(0,400);
+        seqList.display();
+
+    }
+}
