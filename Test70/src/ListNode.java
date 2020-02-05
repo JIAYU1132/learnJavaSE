299b69751d8150693432bb7fa5ce35d1a121704a
yunjiayu <386902834@qq.com>
2020-02-05 09:54:57 +0800

    找出它们的第一个公共结点

---
+++ Test70/src/ListNode.java
@@ -0,0 +1,23 @@
+public class ListNode {
+          int val;
+          ListNode next;
+          ListNode(int x) {
+              val = x;
+              next = null;
+         }
+      }
+
+      class Solution {
+        public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+            if (headA == null || headB == null) {
+                return null;
+            }
+            ListNode pA = headA;
+            ListNode pB = headB;
+            while (pA != pB) {
+                pA = pA == null ? headB : pA.next;
+                pB = pB == null ? headA : pB.next;
+            }
+            return pA;
+        }
+    }
