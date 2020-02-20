74cf75bdd79d478f8210a7ae7c3c27f144107fce
yunjiayu <386902834@qq.com>
2020-02-20 09:13:39 +0800

    字符串转换整数 (Atoi)

---
+++ Test92/src/Test.java
@@ -0,0 +1,51 @@
+public class Test {
+    //字符串转换整数 (Atoi)
+    public int myAtoi(String str) {
+        int rev = 0;
+        char[] charList = str.toCharArray();
+        int length = str.length();
+        int zf = 1;
+        int pop = 0;
+        int i = 0;
+        for(;  i < length; i++){
+            //遇见空格自动跳过
+            if(charList[i] == ' '){
+                continue;
+            }else{
+                //遇见 - 号，给 zf
+                if(charList[i] == '-'){
+                    i++;
+                    zf = -1;
+                    break;
+                }
+                if(charList[i] == '+'){
+                    i++;
+                    break;
+                }
+                if(charList[i] <'0' || charList[i] >'9'){
+                    return 0;
+                }else{
+                    break;
+                }
+            }
+        }
+        if(i == length){
+            return 0;
+        }
+        for(;i < length; i++){
+            if(charList[i] <'0' || charList[i] >'9'){
+                return rev;
+            }
+            pop = (charList[i] -48) * zf;
+            if(rev>Integer.MAX_VALUE/10 || (rev == Integer.MAX_VALUE / 10 && pop > 7)){
+                return Integer.MAX_VALUE;
+            }
+            if(rev<Integer.MIN_VALUE/10 || (rev == Integer.MIN_VALUE / 10 && pop < -8)){
+                return Integer.MIN_VALUE;
+            }
+            rev = rev * 10 + pop;
+        }
+        return rev;
+    }
+
+}
