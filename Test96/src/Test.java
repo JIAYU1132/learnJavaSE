ce26564bb4298dfcb948a06afcf226441642b23c
yunjiayu <386902834@qq.com>
2020-02-23 09:42:08 +0800

    根据逆波兰表示法，求表达式的值(数组实现)

---
+++ Test96/src/Test.java
@@ -0,0 +1,28 @@
+public class Test {
+    //根据逆波兰表示法，求表达式的值。
+    public int evalRPN(String[] tokens) {
+        int[] numStack = new int[tokens.length / 2 + 1];
+        int index = 0;
+        for (String s : tokens) {
+            switch (s) {
+                case "+":
+                    numStack[index - 2] += numStack[--index];
+                    break;
+                case "-":
+                    numStack[index - 2] -= numStack[--index];
+                    break;
+                case "*":
+                    numStack[index - 2] *= numStack[--index];
+                    break;
+                case "/":
+                    numStack[index - 2] /= numStack[--index];
+                    break;
+                default:
+                    numStack[index++] = Integer.parseInt(s);
+                    break;
+            }
+        }
+        return numStack[0];
+
+    }
+}
