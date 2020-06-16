e6ada7e6daf1edbfcb1d4c31aa863bb9495b00cf
yjy <386902834@qq.com>
2020-06-16 14:18:12 +0800

    实现contains, 判定字符串中是否包含子串

---
+++ Test137/src/Test.java
@@ -0,0 +1,8 @@
+public class Test {
+    //实现方法 contains, 能够判定字符串中是否包含子串
+    public static void main(String[] args) {
+        String str = "helloword";
+        System.out.println(str.contains("hello"));
+        System.out.println(str.contains("helow"));
+    }
+}
